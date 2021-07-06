9c356ac505eedf3001af2174e4a2f676fd7a5adb
Arjun Dubey <arjundubeycr@gmail.com>
2021-07-06 23:09:45 +0530

    added team data

--- src/components/Team/Team.vue
+++ src/components/Team/Team.vue
@@ -1,11 +1,38 @@
 <template>
-  <div>
-    <h2>team page</h2>
+  <div class="container">
+    <div class="display-3 fw-bold">Our Team</div>
+    <div class="row">
+      <div class="col" v-for="member in teamdata" :key="member.githubHandle">
+        <div class="card text-center">
+          <img
+            :src="`https://github.com/${member.githubHandle}.png`"
+            class="card-img-top"
+            :alt="`${member.githubHandle}'s Profile Picture`"
+          />
+          <div class="card-body">
+            <h5 class="card-title">{{ member.name }}</h5>
+            <p class="card-text">
+              <a class="btn btn-primary" :href="`https://github.com/${member.githubHandle}`"
+                >View On Github</a
+              >
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import data from './data.json';
+export default {
+  name: 'Team',
+  data() {
+    return {
+      teamdata: data,
+    };
+  },
+};
 </script>
 
 <style scoped></style>
